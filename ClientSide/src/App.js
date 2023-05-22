6bcc4c8d5e3e8cfa337915c8f7ffee163e988457
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-22 21:52:27 +0100

    final

--- ClientSide/src/App.js
+++ ClientSide/src/App.js
@@ -6,9 +6,10 @@ import myEpicNft from "./utils/MyEpicNFT.json";
 // Constants
 const TWITTER_HANDLE = "eketeUg";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = "";
+const OPENSEA_LINK = "https://testnets.opensea.io/collection/squarenft-507";
 const TOTAL_MINT_COUNT = 50;
 
+const CONTRACT_ADDRESS = "0x4C0532939Fe4aac1642D4BD022d49Dba05F4b708";
 const App = () => {
   /*
    * Just a state variable we use to store our user's public wallet. Don't forget to import useState.
@@ -44,6 +45,10 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and ALREADY had their wallet connected + authorized.
+      setupEventListener();
     } else {
       console.log("No authorized account found");
     }
@@ -68,19 +73,64 @@ const App = () => {
         method: "eth_requestAccounts",
       });
 
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      console.log("Connected to chain " + chainId);
+
+      // String, hex code of the chainId of the Goerli test network
+      const goerliChainId = "0x8";
+      if (chainId !== goerliChainId) {
+        alert("You are not connected to the Polygon matic Test Network!");
+      }
       /*
        * Boom! This should print out public address once we authorize Metamask.
        */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x759Dc4c2156144017195Dd723D5f6002C86c08Fb";
-
     try {
       const { ethereum } = window;
 
@@ -131,9 +181,9 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
+          <p className="header gradient-text">Team2 NFT Badge Collections</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+            Each unique. Each beautiful. work hard and claim your NFT today.
           </p>
           {/* Add your render method  and conditionalhere */}
           {currentAccount === "" ? (
