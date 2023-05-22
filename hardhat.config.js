1193258287d326a4cda93b679bbe2637625c2ec1
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-22 11:58:02 +0100

    dynamic svg

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    goerli: {
+    sepolia: {
       url: process.env.QUICKNODE_API_KEY_URL,
       accounts: [process.env.GOERLI_PRIVATE_KEY],
     },
